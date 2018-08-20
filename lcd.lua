9602e86946d0c6dec7a9df43bfb55951a7787575
Nicolas BONNIOT <nicolas@devgiants.fr>
2018-08-20 23:00:05 +0200

    Add return code on init to abort next calls if not OK Also add comments

--- lcd.lua
+++ lcd.lua
@@ -1,11 +1,12 @@
 do
     if(_G['i2c'] ~= nil) then        
+        -- Retrieve constants
         local constants = dofile('constants.lua')
 
         print(constants.MODULE_NAME .. ': I2C here. continue.')
-        -- commands
-        
 
+        -- LOCAL VARIABLES NEEDED
+        -------------------
         local id
         local sda
         local scl
@@ -20,17 +21,20 @@ do
         local displayMode
         local numLines
 
+        -- Write to the I2C expander.
+        -- Initiates I2C connection
         local function expanderWrite(data, backlight)
             if(backlight == nil) then
                 backlight = constants.LCD_BACKLIGHT
             end
             i2c.start(id)
-            i2c.address(id, address ,i2c.TRANSMITTER)  
-            -- Force backlight on write so far         
+            i2c.address(id, address ,i2c.TRANSMITTER)              
             i2c.write(id, bit.bor(data, backlight))
             i2c.stop(id)
         end
 
+        -- According to datasheet p22, 4-bit interface data must be sent twice (high order bits first)
+        -- EN bit is toggling on each sending
         local function pulseEnable(data)
             expanderWrite(bit.bor(data, constants.EN))
             tmr.delay(1)
@@ -38,14 +42,16 @@ do
             tmr.delay(50)
         end                
 
+        -- Write 4 bits with EN toggling
         local function write4bits(value)
             expanderWrite(value)
             pulseEnable(value)
         end   
 
-        -- write either command or data on 4 bits mode
-        local function send(value, mode)
-            print(value)
+        -- Send value with 4 bit splitting
+        -- First high-order bits, then low ones
+        -- mode is supposed to receive 0 for commands, RS value for data
+        local function send(value, mode)            
             local highNib = bit.band(value, 0xF0)
             local lowNib = bit.band(bit.lshift(value, 4), 0xF0)
 
@@ -53,45 +59,53 @@ do
             write4bits(bit.bor(lowNib, mode))
         end
 
+        -- Wrapper to force command 
         local function command(value)
             send(value, 0)
         end
 
+        -- Write to LCD
         local function write(value)
             send(value, constants.RS)
             return 1
         end      
 
+        -- Compute display value and send it to LCD
         local function display()
             displayControl =  bit.bor(displayControl, constants.LCD_DISPLAYON)
             command(bit.bor(constants.LCD_DISPLAYCONTROL, displayControl))
         end
 
+        -- Clear screen
         local function clear()
             command(constants.LCD_CLEARDISPLAY)
             tmr.delay(2000)
         end
 
+        -- Send cursor to home
         local function home()
             command(constants.LCD_RETURNHOME)
             tmr.delay(2000)
         end
 
+        -- Set cursor to line beginning
+        -- Both col and row variables are 0-based
         local function setCursor(col, row)
-            rowOffsets = { 0x00, 0x40, 0x14, 0x54 }
 
-            print(row .. ' ' .. col .. ' ' .. rows)
-            print(rowOffsets[row])                      
-            
+            -- Add make row 1-based for lua array usage
+            row = row + 1
+
+            -- Offsets for 4 rows 
+            rowOffsets = { 0x00, 0x40, 0x14, 0x54 }
+                                
             command(bit.bor(constants.LCD_SETDDRAMADDR, (col + rowOffsets[row])))
         end
 
         local function printString(str)
             for i = 1, #str do
              local char = string.byte(string.sub(str, i, i))
-             print(char)
-             write(char)
-             --send ({ bit.clear(char,0,1,2,3),bit.lshift(bit.clear(char,4,5,6,7),4)})
+             print(constants.MODULE_NAME .. ': Char "' .. string.sub(str, i, i) .. '" represented by ' .. char)
+             write(char)             
             end
         end
 
@@ -102,26 +116,32 @@ do
         end
 
          -- Turn the (optional) backlight off/on
-        local function backlight()
-            backlight = constants.LCD_BACKLIGHT
-            expanderWrite(0, backlight)
+        local function setBacklight(value)
+            if ((value == constants.LCD_BACKLIGHT) or (value == constants.LCD_NOBACKLIGHT)) then
+                expanderWrite(0, value)
+            end            
         end
 
 
         local function init(givenSda, givenScl, givenAddress, givenColsNumber, givenRowsNumber, givenId)
 
+            -- setCursor can't handle offset up to 4 lines
+            if(givenRowsNumber > 4) then
+                print(constants.MODULE_NAME .. ': This library can only handle 4 rows max.')
+                return false
+            end
             -- Assign object var
             if givenId ~= nil then id = givenId else id = 0 end
             address = givenAddress
             sda = givenSda
             scl = givenScl
 
-            cols = givenColssNumber
+            cols = givenColsNumber
             rows = givenRowsNumber            
 
-            displayFunction = bit.bor(constants.LCD_4BITMODE, constants.LCD_1LINE, constants.LCD_5x8DOTS)           
-            --print(constants.MODULE_NAME .. ': displayFunction value ' .. displayFunction)
+            displayFunction = bit.bor(constants.LCD_4BITMODE, constants.LCD_1LINE, constants.LCD_5x8DOTS)                      
 
+            -- Activate 2-lines LCD if more than one row
             if rows > 1 then
                 displayFunction = bit.bor(displayFunction, constants.LCD_2LINE)
             end
@@ -133,38 +153,31 @@ do
             -- Establishing connection
             local speed = i2c.setup(id, sda, scl, i2c.SLOW)
             print(constants.MODULE_NAME .. ': I2C setup (SDA : ' .. sda .. ', SCL : ' .. scl .. ', device at 0x' .. string.format('%x', address) .. ') at ' .. speed .. ' bit/s.')
-
-            backlight = constants.LCD_NOBACKLIGHT
-            print(backlight)
-           -- expanderWrite(backlight)
-
-            -- What to do with that
-            tmr.delay(1000000)
+                    
+            -- Sequence described p45 of datasheet
+            --------------------------------------
             
-            -- we start in 8bit mode, try to set 4 bit mode
+            
             write4bits(bit.lshift(0x03, 4))
             tmr.delay(4500) -- wait min 4.1ms
-   
-            -- second try
+               
             write4bits(bit.lshift(0x03, 4))
             tmr.delay(4500);  -- wait min 4.1ms
-   
-            -- third go!
+     
             write4bits(bit.lshift(0x03, 4))
             tmr.delay(150);
    
             -- finally, set to 4-bit interface
             write4bits(bit.lshift(0x02, 4))
 
-
             -- set # lines, font size, etc.
-            command(bit.bor(constants.LCD_FUNCTIONSET, displayFunction))
-            
-
+            command(bit.bor(constants.LCD_FUNCTIONSET, displayFunction))           
             displayControl = bit.bor(constants.LCD_DISPLAYON, constants.LCD_CURSOROFF, constants.LCD_BLINKOFF)
-            
-            display()
 
+            -- Compute display
+            display()
+            
+            -- Clear screen
             clear()
 
             --Initialize to default text direction (for roman languages)
@@ -173,17 +186,25 @@ do
             -- set the entry mode
             command(bit.bor(constants.LCD_ENTRYMODESET, displayMode))
 
+            -- Cursor to home
             home()
-            
+
+            -- All right
+            return true
         end
       
         -- Define object
         hd44780 = {
-            init=init,
-            noBacklight=noBacklight,          
-            backlight=backlight,
-            setCursor=setCursor,
-            printString=printString
+            -- Constants available when using object
+            BACKLIGHT_ON = constants.LCD_BACKLIGHT,
+            BACKLIGHT_OFF = constants.LCD_NOBACKLIGHT,
+
+            -- Methods
+            init = init,
+            setBacklight = setBacklight,
+            setCursor = setCursor,
+            clear = clear,
+            printString = printString            
         }
     else
         print(constants.MODULE_NAME .. ': I2C module is required to run this library. Please include it in your firmware.')
